dd63d56028b390c7067fde3ab34f9105ff7ad99e
Cristian Gomez <cristiangomezmendez@gmail.com>
2018-07-05 12:56:19 -0400

    implementing endless scroll to be able to paginate

---
+++ app/src/main/java/me/cristiangomez/news/util/EndLessScrollListener.java
@@ -0,0 +1,58 @@
+package me.cristiangomez.news.util;
+
+import android.widget.AbsListView;
+
+public abstract class EndLessScrollListener implements AbsListView.OnScrollListener {
+    private int visibleThreshold = 5;
+    private int currentPage = 0;
+    private int previousTotalItemCount = 0;
+    private boolean loading = true;
+    private int startingPageIndex = 0;
+
+    public EndLessScrollListener() {
+    }
+
+    public EndLessScrollListener(int visibleThreshold) {
+        this.visibleThreshold = visibleThreshold;
+    }
+
+    public EndLessScrollListener(int visibleThreshold, int startPage) {
+        this.visibleThreshold = visibleThreshold;
+        this.startingPageIndex = startPage;
+        this.currentPage = startPage;
+    }
+
+    @Override
+    public void onScrollStateChanged(AbsListView absListView, int i) {
+
+    }
+
+    @Override
+    public void onScroll(AbsListView absListView, int firstVisibleItem, int visibleItemCount,
+                         int totalItemCount) {
+        if (totalItemCount < previousTotalItemCount) {
+            this.currentPage = this.startingPageIndex;
+            this.previousTotalItemCount = totalItemCount;
+            if (totalItemCount < previousTotalItemCount) {
+                this.currentPage = this.startingPageIndex;
+                this.previousTotalItemCount = totalItemCount;
+                if (totalItemCount == 0) {
+                    this.loading = true;
+                }
+            }
+        }
+
+        if (loading && (totalItemCount > previousTotalItemCount)) {
+            loading = false;
+            previousTotalItemCount = totalItemCount;
+            currentPage++;
+        }
+
+
+        if (!loading && (firstVisibleItem + visibleItemCount + visibleThreshold) >= totalItemCount) {
+            loading = onLoadMore(currentPage + 1, totalItemCount);
+        }
+    }
+
+    public abstract boolean onLoadMore(int page, int totalItemCount);
+}
