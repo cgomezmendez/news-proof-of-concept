607eac7582be0d2e4e157510ce9b21affdc5e0cb
Cristian Gomez <cristiangomezmendez@gmail.com>
2018-07-05 14:37:38 -0400

    improving ui adding loading progress bar when image is being downloaded.

--- app/src/main/java/me/cristiangomez/news/feed/FeedListAdapter.java
+++ app/src/main/java/me/cristiangomez/news/feed/FeedListAdapter.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -49,30 +50,35 @@ public class FeedListAdapter extends ArrayAdapter<Story> {
         private ImageView thumbnail;
         private TextView title;
         private TextView summary;
+        private ProgressBar progressBar;
         private Uri imageUri;
 
         ViewHolder(View view) {
             this.thumbnail = view.findViewById(R.id.story_thumbnail);
             this.title = view.findViewById(R.id.story_title);
             this.summary = view.findViewById(R.id.story_summary);
+            this.progressBar = view.findViewById(R.id.story_thumbnail_progress_bar);
         }
 
         void bind(Story story) {
             this.title.setText(story.getHeadLine());
             this.summary.setText(Html.fromHtml(story.getTrailText()));
             if (story.getThumbnail() != null) {
-                this.thumbnail.setImageBitmap(null);
                 this.imageUri = story.getThumbnail();
+                this.progressBar.setVisibility(View.VISIBLE);
+                this.thumbnail.setImageBitmap(null);
                 new ImageDownloadTask(new ImageDownloadTask.ImageDownloadCallback() {
                     @Override
                     public void onImageDownloaded(Image image) {
                         if (image != null && image.getBitmap() != null
                                 && image.getUri() == ViewHolder.this.imageUri) {
+                            ViewHolder.this.progressBar.setVisibility(View.INVISIBLE);
                             ViewHolder.this.thumbnail.setImageBitmap(image.getBitmap());
                         }
                     }
                 }).execute(story.getThumbnail());
             } else {
+                this.progressBar.setVisibility(View.INVISIBLE);
                 this.imageUri = null;
                 this.thumbnail.setImageBitmap(null);
             }
