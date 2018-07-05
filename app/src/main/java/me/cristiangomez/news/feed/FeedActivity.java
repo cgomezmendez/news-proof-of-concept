06fb0f7aa6838f0191ed666022f3cc9d2b7e967b
Cristian Gomez <cristiangomezmendez@gmail.com>
2018-07-04 22:57:57 -0400

    header implementation

--- app/src/main/java/me/cristiangomez/news/feed/FeedActivity.java
+++ app/src/main/java/me/cristiangomez/news/feed/FeedActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import me.cristiangomez.news.R;
 
 public class FeedActivity extends AppCompatActivity {
@@ -13,6 +14,8 @@ public class FeedActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.feed_act);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         FeedFragment feedFragment = (FeedFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.feed_content);
         feedPresenter = new FeedPresenter(feedFragment);
