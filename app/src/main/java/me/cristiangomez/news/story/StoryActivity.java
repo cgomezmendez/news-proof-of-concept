6284458f8d700270042bb8b80a4a68d2a8eec8dc
Cristian Gomez <cristiangomezmendez@gmail.com>
2018-07-06 13:40:14 -0400

    single story ui

--- app/src/main/java/me/cristiangomez/news/story/StoryActivity.java
+++ app/src/main/java/me/cristiangomez/news/story/StoryActivity.java
@@ -1,14 +1,18 @@
 package me.cristiangomez.news.story;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import me.cristiangomez.news.R;
 
 public class StoryActivity extends AppCompatActivity {
     public static final String EXTRA_STORY_ID = "story_id";
     private StoryPresenter storyPresenter;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -17,6 +21,25 @@ public class StoryActivity extends AppCompatActivity {
         StoryFragment storyFragment = (StoryFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.story_content);
         String storyID = getIntent().getStringExtra(EXTRA_STORY_ID);
-        storyPresenter = new StoryPresenter(storyFragment, storyID);
+        if (storyFragment != null) {
+            storyPresenter = new StoryPresenter(storyFragment, storyID);
+        }
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setBackgroundColor(Color.TRANSPARENT);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setTitle(null);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.homeAsUp) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
