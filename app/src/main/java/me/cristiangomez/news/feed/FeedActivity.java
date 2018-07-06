2b31d9051d4690c16f812b10cc07aac46615cc07
Cristian Gomez <cristiangomezmendez@gmail.com>
2018-07-06 16:31:53 -0400

    mmisng commit in fragment transaction

--- app/src/main/java/me/cristiangomez/news/feed/FeedActivity.java
+++ app/src/main/java/me/cristiangomez/news/feed/FeedActivity.java
@@ -91,7 +91,8 @@ public class FeedActivity extends AppCompatActivity {
     private void showSection(String section) {
         FeedFragment feedFragment = new FeedFragment();
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.feed_content, feedFragment);
+                .replace(R.id.feed_content, feedFragment)
+        .commit();
         feedPresenter = new FeedPresenter(feedFragment, section);
     }
 }
